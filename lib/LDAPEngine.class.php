d59c945249414ff57c86cf26182fb61a16b990c7
lqqkout13 <lqqkout13@12d54735-0ee2-4768-9b18-c71e5e561e4c>
2006-03-29 15:02:30 +0000

    Changes to ldap search for uid

--- lib/LDAPEngine.class.php
+++ lib/LDAPEngine.class.php
@@ -2,7 +2,7 @@
 /**
 * LDAPEngine class
 * @author David Poole <David.Poole@fccc.edu>
-* @version 04-19-05
+* @version 03-29-06
 * @package LDAPEngine
 *
 * Copyright (C) 2004 phpScheduleIt
@@ -23,62 +23,72 @@ class LDAPEngine {
 	var $host;
 	var $port;
 	var $basedn;
-	
+
 	var $binddn;
     var $ldap;
     var $connected;
-    
+
     var $uid;
     var $fname;
     var $lname;
     var $mail;
     var $phone;
 	var $password;
-    
+
 	/**
 	* LDAPEngine constructor to initialize object
 	* @param string $uid user id
 	* @param string $password password associated with uid
 	*/
 	function LDAPEngine( $uid, $password ) {
-	   global $conf;
-	   
-	   $this->connected = false;
-	   
-	   if( strlen( $uid ) == 0 || strlen( $password ) == 0 ) {
-	   	  return;
-	   }
-	   
-	   $this->host = $conf['ldap']['host'];
-	   $this->port = $conf['ldap']['port'];
-	   $this->basedn = $conf['ldap']['basedn'];
-	   
-	   $this->ldap = ldap_connect( $this->host, $this->port ) or die( "Could not connect to LDAP server." );
-	   
-	   $this->uid = $uid;
-	   $uid = "uid=$uid,". $this->basedn;
-	   
+		global $conf;
+
+		$this->connected = false;
+
+		if( strlen( $uid ) == 0 || strlen( $password ) == 0 ) {
+			return;
+		}
+
+		$this->host = $conf['ldap']['host'];
+		$this->port = $conf['ldap']['port'];
+		$this->basedn = $conf['ldap']['basedn'];
+
+		$this->ldap = ldap_connect( $this->host, $this->port ) or die( "Could not connect to LDAP server." );
+
+		$this->uid = $uid;
+		$uid = "uid=$uid,". $this->basedn;
+
+		if( $rdnSearch = @ldap_bind( $this->ldap ) ) {
+	   		if( $resID = ldap_search( $this->ldap, $this->basedn, "uid=$uid" ) ) {
+				if( ldap_count_entries( $this->ldap, $resID ) == 1 ) {
+					if( $firstEntry = ldap_first_entry( $this->ldap, $resID ) ) {
+						$uid = ldap_get_dn( $this->ldap, $firstEntry );
+					}
+				}
+			}
+		}
+
 	   if( $this->ldap ) {
 	       $bind = @ldap_bind( $this->ldap, $uid, $password );
 
 	       if( $bind ) {
 	           // authenication was a success, try to load user information
 	           $this->binddn = $uid;
-	           
+
 	           if( $this->loadUserData() ) {
 	               $this->connected = true;
 	               $this->password = $password;
 	           } else {
 	               ldap_close( $this->ldap );
 	           }
-            
+
             } else {
                 ldap_close( $this->ldap );
             }
         }
 
     }
-    
+
     /**
 	* Disconnects from the LDAP server
 	* @param none
@@ -87,39 +97,39 @@ class LDAPEngine {
         if( $this->connected ) {
             ldap_close( $this->ldap );
             $this->connected = false;
-        } 
+        }
     }
 
 	/**
-	* Queries LDAP for user information 
+	* Queries LDAP for user information
 	* @return boolean indicating success or failure
 	*/
 	function loadUserData() {
 
 		$attributes = array( "sn", "givenname", "mail", "telephonenumber" );
-		
+
         $result = ldap_search( $this->ldap, $this->binddn, "uid=". $this->uid, $attributes );
         $entries = ldap_get_entries( $this->ldap, $result );
-        
-        if( $result and ( $entries["count"] == 1 ) ) {        
+
+        if( $result and ( $entries["count"] == 1 ) ) {
             $this->fname = $entries[0]['givenname'][0];
             $this->lname = $entries[0]['sn'][0];
             $this->mail = strtolower( $entries[0]['mail'][0] );
             $this->phone = $entries[0]['telephonenumber'][0];
-        } else {    
+        } else {
             return false;
         }
-	
+
 	   return true;
-	
+
 	}
-	
+
     /**
-	* Returns user information 
+	* Returns user information
 	* @return array containing user information
 	*/
     function getUserData() {
-    
+
         $return = array(
             'fname' => $this->fname,
             'lname' => $this->lname,
@@ -129,18 +139,18 @@ class LDAPEngine {
             'password' => $this->password,
             'password2' => $this->password
         );
-        
+
         return $return;
-    
+
     }
-    	
+
     /**
 	* Gets user's mail attribute.
 	* @return string user's email address
 	*/
 	function getUserEmail( ) {
 	  	return $this->mail;
-    }	
+    }
 
     /**
 	* See if the user was authenticated.
@@ -148,7 +158,7 @@ class LDAPEngine {
 	*/
 	function connected( ) {
 	  	return $this->connected;
-    }	
+    }
 
 }
 
