8e37ddd7fd5f2f7e97d1cc46665c0e4ab3a84e2c
lqqkout13 <lqqkout13@12d54735-0ee2-4768-9b18-c71e5e561e4c>
2006-04-13 14:46:58 +0000

    iCal support completed

--- lib/interfaces/IReservationExport.php
+++ lib/interfaces/IReservationExport.php
@@ -35,7 +35,7 @@ class IReservationFormatter
 class ICalReservationFormatter
 {
 	var $_reservation;
-	
+
 
 	function ICalReservationFormatter(&$reservation) {
 		$this->_reservation = $reservation;
@@ -43,40 +43,74 @@ class ICalReservationFormatter
 
 	function format() {
 		$builder = new StringBuilder();
-		
+
 		$builder->append("BEGIN:VEVENT\n");
 		$builder->append("END:VEVENT\n");
-		
+
 		return $builder->toString();
 	}
 
 	function formatSettings() {
 		$builder = new StringBuilder();
-		
+
 		$builder->append("UID:{$this->_reservation->id}\n");
 		$builder->append(sprintf("DTSTART:%sT%s%s00Z\n", date('Ymd', $this->_reservation->start_date), Time::getHours($this->_reservation->start), Time::getMinutes($this->_reservation->start)) );
 		$builder->append(sprintf("DTEND:%sT%s%s00Z\n", date('Ymd', $this->_reservation->end_date), Time::getHours($this->_reservation->end), Time::getMinutes($this->_reservation->end)) );
 		$builder->append(sprintf("CREATED:%sT%sZ\n", date('Ymd', $this->_reservation->created), date('His', $this->_reservation->created)));
 		$builder->append(sprintf("LAST-MODIFIED:%sT%sZ\n", date('Ymd', $this->_reservation->modified), date('His', $this->_reservation->modified)));
-		
+
 		return $builder->toString();
 	}
-	/*
-	function formatOwner() {
 
+	function formatOwner() {
+		$builder = new StringBuilder();
+
+		$builder->append("ORGANIZER:MAILTO:{$this->_reservation->user->email}\n");
+
+		return $builder->toString();
 	}
 
 	function formatParticipants() {
+		$builder = new StringBuilder();
 
+		for ($i = 0; $i < count($this->_reservation->users); $i++) {
+			$builder->append("ATTENDEE:MAILTO:{$this->_reservation->users[$i]['email']}\n");
+		}
+
+		return $builder->toString();
+	}
+
+	function formatSummary() {
+		$builder = new StringBuilder();
+
+		$builder->append("SUMMARY:{$this->_reservation->summary}\n");
+
+		return $builder->toString();
 	}
 
 	function formatReminder() {
+		$builder = new StringBuilder();
 
+		$builder->append("BEGIN:VALARM\n");
+		$builder->append("ACTION:EMAIL\n");
+		$builder->append("TRIGGER:-P{$this->_reservation->reminder}M\n");
+		$builder->append("END:VALARM\n");
+
+		return $builder->toString();
 	}
 
 	function formatResources() {
+		$builder = new StringBuilder();
 
+		$builder->append("RESOURCES:{$this->_reservation->resource->name}");
+
+		for ($i = 0; $i < count($this->_reservation->resources); $i++) {
+			$builder->append(",$this->_reservation->resources[$i]['name']");
+		}
+
+		$builder->append("\nLOCATION:{$this->_reservation->resource->location}\n");
+
+		return $builder->toString();
 	}
-	*/
 }
 ?>
--- tests/ICalFormatterTests.php
+++ tests/ICalFormatterTests.php
@@ -8,33 +8,78 @@ class ICalFormatterTests extends PHPUnit_TestCase
 	function ICalFormatterTests($name) {
 		$this->PHPUnit_TestCase($name);
 	}
-	
-	function testReservationIsFormattedToICalSpecs() {
+
+	function testFormatSettingsIncludesProperAttributes() {
 		$fake = new FakeReservation();
-		
+
 		$formatter = new ICalReservationFormatter($fake);
 		$settings = $formatter->formatSettings();
-		
+
 		$id = $fake->id;
-		
+
 		$dtstart = sprintf(
-			'%sT%s%s00Z', 
-			date('Ymd', $fake->start_date), 
+			'%sT%s%s00Z',
+			date('Ymd', $fake->start_date),
 			Time::getHours($fake->start),
 			Time::getMinutes($fake->start)
 		);
-		
+
 		$dtend = sprintf(
-			'%sT%s%s00Z', 
-			date('Ymd', $fake->end_date), 
+			'%sT%s%s00Z',
+			date('Ymd', $fake->end_date),
 			Time::getHours($fake->end),
 			Time::getMinutes($fake->end)
 		);
-		
+
 		$created = sprintf('%sT%sZ', date('Ymd', $fake->created), date('His', $fake->created));
 		$modified = sprintf('%sT%sZ', date('Ymd', $fake->modified), date('His', $fake->modified));
-		
+
 		$this->assertEquals("UID:$id\nDTSTART:$dtstart\nDTEND:$dtend\nCREATED:$created\nLAST-MODIFIED:$modified\n", $settings);
+	}
+
+	function testFormatOwnerIncludesProperAttributes() {
+		$fake = new FakeReservation();
+
+		$formatter = new ICalReservationFormatter($fake);
+		$owner = $formatter->formatOwner();
+
+		$this->assertEquals("ORGANIZER:MAILTO:test@email.com\n", $owner);
+	}
+
+	function testFormatParticipantsIncludesProperAttributes() {
+		$fake = new FakeReservation();
+
+		$formatter = new ICalReservationFormatter($fake);
+		$participants = $formatter->formatParticipants();
+
+		$this->assertEquals("ATTENDEE:MAILTO:fake1@email.com\nATTENDEE:MAILTO:fake2@email.com\n", $participants);
+	}
+
+	function testFormatSummaryIncludesProperAttributes() {
+		$fake = new FakeReservation();
+
+		$formatter = new ICalReservationFormatter($fake);
+		$summary = $formatter->formatSummary();
+
+		$this->assertEquals("SUMMARY:summary\n", $summary);
+	}
+
+	function testFormatReminderIncludesProperAttributes() {
+		$fake = new FakeReservation();
+
+		$formatter = new ICalReservationFormatter($fake);
+		$reminder = $formatter->formatReminder();
+
+		$this->assertEquals("BEGIN:VALARM\nACTION:EMAIL\nTRIGGER:-P20M\nEND:VALARM\n", $reminder);
+	}
+
+	function testFormatResourcesncludesProperAttributes() {
+		$fake = new FakeReservation();
+
+		$formatter = new ICalReservationFormatter($fake);
+		$resources = $formatter->formatResources();
+
+		$this->assertEquals("RESOURCES:resource1,projector1\nLOCATION:location1\n", $resources);
 	}
 }
 ?>
