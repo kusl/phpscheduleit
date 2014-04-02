b3e93bb6ba968868de1728354cd60e8024e9093e
lqqkout13 <lqqkout13@12d54735-0ee2-4768-9b18-c71e5e561e4c>
2014-04-02 18:16:38 +0000

    fixed manage reservations/resources custom attribute filter when multiple attributes are provided

--- 2.5/lib/Application/Admin/ReservationFilter.php
+++ 2.5/lib/Application/Admin/ReservationFilter.php
@@ -149,7 +149,9 @@ class ReservationFilter
 		}
 		if (!empty($this->attributes))
 		{
-			$f  = new SqlFilterFreeForm(TableNames::RESERVATION_SERIES_ALIAS . '.' . ColumnNames::SERIES_ID . ' IN (SELECT ' . ColumnNames::ATTRIBUTE_ENTITY_ID . ' FROM ' . TableNames::CUSTOM_ATTRIBUTE_VALUES . ' WHERE [attribute_list_token] )');
+			$filteringAttributes = false;
+
+			$f  = new SqlFilterFreeForm(TableNames::RESERVATION_SERIES_ALIAS . '.' . ColumnNames::SERIES_ID . ' IN (SELECT a0.' . ColumnNames::ATTRIBUTE_ENTITY_ID . ' FROM ' . TableNames::CUSTOM_ATTRIBUTE_VALUES . ' a0 ');// . ' WHERE [attribute_list_token] )');
 
 			$attributeFragment = new SqlFilterNull();
 
@@ -160,25 +162,30 @@ class ReservationFilter
 				{
 					continue;
 				}
-				$attributeId = new SqlRepeatingFilterColumn(null, ColumnNames::CUSTOM_ATTRIBUTE_ID, $i);
-				$attributeValue = new SqlRepeatingFilterColumn(null, ColumnNames::CUSTOM_ATTRIBUTE_VALUE, $i);
+				$id = $attribute->Id();
+				$filteringAttributes = true;
+				$attributeId = new SqlRepeatingFilterColumn("a$id", ColumnNames::CUSTOM_ATTRIBUTE_ID, $id);
+				$attributeValue = new SqlRepeatingFilterColumn("a$id", ColumnNames::CUSTOM_ATTRIBUTE_VALUE, $id);
 
-				$id = new SqlFilterEquals($attributeId, $attribute->Id());
-
+				$idEquals = new SqlFilterEquals($attributeId, $attribute->Id());
+				$f->AppendSql('LEFT JOIN ' . TableNames::CUSTOM_ATTRIBUTE_VALUES . ' a' . $id . ' ON a0.entity_id = a' . $id . '.entity_id ');
 				if ($attribute->Type() == CustomAttributeTypes::MULTI_LINE_TEXTBOX || $attribute->Type() == CustomAttributeTypes::SINGLE_LINE_TEXTBOX)
 				{
-					$attributeFragment->_And($id->_And(new SqlFilterLike($attributeValue, $attribute->Value())));
+					$attributeFragment->_And($idEquals->_And(new SqlFilterLike($attributeValue, $attribute->Value())));
 				}
 				else
 				{
-					$attributeFragment->_And($id->_And(new SqlFilterEquals($attributeValue, $attribute->Value())));
+					$attributeFragment->_And($idEquals->_And(new SqlFilterEquals($attributeValue, $attribute->Value())));
 				}
 			}
 
+			$f->AppendSql("WHERE [attribute_list_token] )");
 			$f->Substitute('attribute_list_token', $attributeFragment);
 
-
-			$filter->_And($f);
+			if ($filteringAttributes)
+			{
+				$filter->_And($f);
+			}
 		}
 
 		foreach ($this->_and as $and)
